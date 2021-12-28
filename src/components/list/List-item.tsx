59ec1832ba61db03ffdb2d52a015e8c3f825204c
unknown <kimminhyug@naver.com>
2021-12-28 20:04:25 +0900

    list item 틀만 잡아 추가함. hover, animation 적용, 일부 사이즈에 맞춰 반응형 작업

--- src/components/list/List-item.tsx
+++ src/components/list/List-item.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { HTMLAttributes, HtmlHTMLAttributes, useEffect, useRef, useState } from 'react';
 import '../../scss/List.scss';
 import '../../scss/Champion.scss';
 
@@ -16,6 +16,8 @@ const getChampionPicture = (championName:string) => {
 
 function ListItem(props:listProps) {
   const championPicture =  getChampionPicture(props.userInfo.championName);
+  const listItemContent = useRef(null);
+  const [isAnimation, setIsAnimation] = useState<boolean>(false);
   let teams:any={};
 
   const filterTeamInMatchInfo = (participants:MatchPlayerInfo[]) => {
@@ -56,35 +58,78 @@ function ListItem(props:listProps) {
     return renderData;
   }
 
-  
+  const renderListItem = ()=>{
+    return animationElment;
+  }
+  const animationElment = [
+    <div className={`${isAnimation?'list-item-content-enable':'list-item-content-disable'}`} ref={listItemContent}>
+      {props.match.info.gameId}
+    </div>
+  ]
+  const onClickTest = ()=>{
+    // document.querySelector(".list-item-content").style.display
+    // .toggle('active');
+    // animationElment
+    
+    let listItem = listItemContent.current;
+    if(!listItem) return;
+    console.log((listItem as HTMLDivElement).className);
+    const className = (listItem as HTMLDivElement).className;
+    switch (className) {
+      case 'list-item-content-enable':
+        setIsAnimation(false)
+        break;
+      case 'list-item-content-disable':
+        setIsAnimation(true)
+        break;
+      default:
+        // setIsAnimation(false)
+        break;
+    }
+    
+    // console.log(listItem)
+    // console.log((listItem as HTMLDivElement).style.display)
+    // (listItem as unknown as HTMLDivElement).style.display = "block";
+
+  }
 
   return (
-    <div className={props.userInfo.win?"List-Item Win":"List-Item Lose"}> 
-      <div className="Champion-Header">
-        <img src={championPicture||'error.png'} title={props.userInfo.championName} alt={props.userInfo.championName} ></img> <br/>
-        <span>{props.userInfo.championName}</span>
-      </div>
+    <div className='itemContainer'>
+      <div className={props.userInfo.win?"List-Item Win":"List-Item Lose"} onClick={onClickTest}> 
+        <div className="Champion-Header">
+          <img src={championPicture||'error.png'} title={props.userInfo.championName} alt={props.userInfo.championName} ></img> <br/>
+          <span>{props.userInfo.championName}</span>
+        </div>
 
-      <div className="Champion-kda">
-        <span> {props.userInfo.kills} /</span> 
-        <span> {props.userInfo.deaths} /</span>
-        <span> {props.userInfo.assists} </span> <br/>
-        <span> {props.userInfo.win?'승리':'패배'} </span>
-      </div>
+        <div className="Champion-kda">
+          <span> {props.userInfo.kills} /</span> 
+          <span> {props.userInfo.deaths} /</span>
+          <span> {props.userInfo.assists} </span> <br/>
+          <span> {props.userInfo.win?'승리':'패배'} </span>
+        </div>
 
-      <div className="Champion-css">
-        <span> 미니언 : {props.userInfo.totalMinionsKilled}</span> <br/>
-        <span> 정글  : {props.userInfo.neutralMinionsKilled}</span> <br/>
-        <span> 총 CS : {props.userInfo.totalMinionsKilled + props.userInfo.neutralMinionsKilled}</span> 
-      </div>
+        <div className="Champion-css">
+          <span> 미니언 : {props.userInfo.totalMinionsKilled}</span> <br/>
+          <span> 정글  : {props.userInfo.neutralMinionsKilled}</span> <br/>
+          <span> 총 CS : {props.userInfo.totalMinionsKilled + props.userInfo.neutralMinionsKilled}</span> 
+        </div>
 
-      <div className="Team">
-        {renderingTeam("100")}
-      </div>
+        <div className="Team">
+          {renderingTeam("100")}
+        </div>
 
-      <div className="Team">
-        {renderingTeam("200")}
+        <div className="Team">
+          {renderingTeam("200")}
+        </div>
+        <div className="list-item-click">
+          <span><b>click !!</b></span>
+        </div>
       </div>
+      {/* <div className="list-item-content">
+            a<br></br>
+            a<br></br>a<br></br>
+      </div> */}
+      {renderListItem()}
     </div>
   );
 }
